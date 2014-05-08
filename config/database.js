baf2d45023c1735537dcf92f8bf567989da0b47f
Guy Vider <guy@guyvider.com>
2014-05-08 09:44:22 -0700

    added comment

--- config/database.js
+++ config/database.js
@@ -3,6 +3,8 @@
 */
 'use strict';
 
+//add any other database configuration fields you may need: user/password/port etc
+//use them in app.js to connect.
 module.exports = {
 	url: 'mongodb://localhost/express-passport'
 };
